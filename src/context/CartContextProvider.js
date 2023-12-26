73c22732a28551f6ad4757683066f9e2b9acb90d
Kodaskora <tomas.kutniauskis@gmail.com>
2023-12-26 18:11:43 +0100

    ok3

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -27,12 +27,11 @@ const cartReducer = (state, action) => {
       };
       updatedItems = [...state.items];
       updatedItems[existingCartItemIndex] = updatedItem;
-    }else{
+    } else {
       updatedItem = {
-        ...action.item
-      }
+        ...action.item,
+      };
       updatedItems = state.items.concat(updatedItem);
-
     }
 
     return {
@@ -40,6 +39,34 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const existingCartItemIndex = state.items.findIndex((item) => {
+      return item.id === action.id;
+    });
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    console.log(existingCartItem);
+
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
